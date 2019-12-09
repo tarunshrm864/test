ee6d631165782a945c8aa7ea62965e04def59931
tarunshrm864 <tarunshrm864@gmail.com>
2019-12-09 11:05:53 +0530

    Updates

---
+++ precache-manifest.ae92690f3f8732ecd5283486557aa773.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b46097e9e1a5152916aae81607870f80",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "014041d2cf5dc85cecb6",
+    "url": "/test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b9386138d766d2bbede8",
+    "url": "/test/static/js/2.9747f88b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test/static/js/2.9747f88b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "014041d2cf5dc85cecb6",
+    "url": "/test/static/js/main.3b1047a3.chunk.js"
+  },
+  {
+    "revision": "1bd2bf14f6f58b53e5ca",
+    "url": "/test/static/js/runtime-main.e5e99524.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
